f2a522ea24397f2acad84d397e4755ba8730e018
Damain Ross <damain.ross@mymona.uwi.edu>
2019-03-05 00:08:28 +0000

    secod commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash
 
 class UserProfile(db.Model):
     # You can use this to change the table name. The default convention is to use
@@ -12,6 +12,17 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
+    
+    
+    def __init__(self, first_name, last_name, username, password):
+
+        id = db.Column(db.Integer, primary_key=True)
+        first_name = db.Column(db.String(80))
+        last_name = db.Column(db.String(80))
+        username = db.Column(db.String(80), unique=True)
+        password = db.Column(db.String(255)) 
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
 
     def is_authenticated(self):
         return True
